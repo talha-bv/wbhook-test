6f039f067d6c90ad5bc2c995082a211b6e1eae31
talha work <talha.khan@bevycommerce.com>
2024-09-26 18:48:58 +0600

    fetching metafield values

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -26,6 +26,36 @@ export const action = async ({ request }) => {
         console.log("---draft order created or updated----");
         console.log("draft_order/create: ", payload.admin_graphql_api_id);
         console.log("draft_order/create: ", payload.name);
+        const draftOrderId = payload.admin_graphql_api_id
+        const metafieldsQuery = 
+        `{
+          draftOrder(id: "${draftOrderId}") {
+            id
+            name
+            metafields(first: 10) {
+              edges {
+                node {
+                  id
+                  namespace
+                  key
+                  value
+                  description
+                }
+              }
+            }
+          }
+        }`;
+        try {
+            const response = await admin.graphql(metafieldsQuery);
+            const responseJson = await response.json();
+            // console.log(responseJson);
+            const metafields = responseJson?.data?.draftOrder?.metafields?.edges?.map(edge => edge.node);
+            console.log("---Draft Order Metafields----");
+            console.log(metafields);
+            console.log("---Draft Order Metafields Fetched----");
+        } catch (error) {
+            console.error("Error fetching metafields: ", error);
+        }
         console.log("---draft order created or updated----");
         break;
     case "CUSTOMERS_REDACT":
